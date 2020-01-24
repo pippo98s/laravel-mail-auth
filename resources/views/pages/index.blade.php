a200a51b640c23e6240f64fc5e720616065fa3c7
pippo98s <filippo5scarcella@gmail.com>
2020-01-24 16:40:59 +0100

    BONUS: mostrare/nascondere i link alle pagine riservate a utenti loggati in base allo stato dell'utente

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -8,19 +8,25 @@
       
       <h1>{{ $category->name }}</h1>
       <p>{{ $category->body }}</p>
-      <span>
-      <a href="{{route('category.delete' , $category-> id)}}">DELETE</a>
-      <span class="hide"></span>
-      <a href="{{route ('category.edit' , $category -> id) }}">EDIT</a>
-      <span class="hide"></span>
-      <a href="{{route ('post.create', $category -> id) }}">POST</a>
-      </span>
+      @auth
+        <span>
+          <a href="{{route('category.delete' , $category-> id)}}">DELETE</a>
+          <span class="hide"></span>
+          <a href="{{route ('category.edit' , $category -> id) }}">EDIT</a>
+          <span class="hide"></span>
+          <a href="{{route ('post.create', $category -> id) }}">POST</a>
+        </span>
+      @endauth
       <div class="posts">
         @foreach ($category->posts as $post)
           <div class="post">
-            <a href="{{ route('post.show' , $post -> id)}}">
-              <h3>{{ $post->title }}</h3>
-            </a>
+            @auth
+              <a href="{{ route('post.show' , $post -> id)}}">
+                <h3>{{ $post->title }}</h3>
+              </a>
+              @else
+                <h3>{{ $post->title }}</h3>
+            @endauth
           </div>
         @endforeach
       </div>
